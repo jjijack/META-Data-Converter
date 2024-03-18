98a470e0819fb3ca0040c99e67201edb0f07f1bf
jjijack <jjijack@163.com>
2024-03-19 03:06:10 +0800

    Compose test&train adt data

--- data_generate.py
+++ data_generate.py
@@ -2,6 +2,7 @@ import numpy as np
 from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 import pandas as pd
+from datetime import datetime
 
 SLA=Dataset('../copernicus/cmems_1999-01-01~2018-12-31.nc')
 SLA_test=Dataset('../copernicus/cmems_2019-01-01~2019-12-31.nc')
@@ -19,7 +20,7 @@ grid_all_test={}
 for key in grid_acs_test.keys():
     grid_all_test[key]=grid_acs_test[key]+grid_acl_test[key]+grid_cs_test[key]+grid_cl_test[key]
 
-np.save('./Data/grid_all_test.npy',grid_all_test._getvalue())
+np.save('./Data/grid_all_test.npy',grid_all_test)
 
 '''--------------------Train Grid Data--------------------'''
 grid_acs=np.load('./Data/grid_acs.npy',allow_pickle=True).item()
@@ -31,6 +32,26 @@ grid_all_train={}
 for key in grid_acs.keys():
     grid_all_train[key]=grid_acs[key]+grid_acl[key]+grid_cs[key]+grid_cl[key]
 
-np.save('./Data/grid_all_train.npy',grid_all_train._getvalue())
+np.save('./Data/grid_all_train.npy',grid_all_train)
 
 '''--------------------Test ADT Data--------------------'''
+adt_test=SLA_test['adt'][:]
+time_test=SLA_test['time'][:]
+adt_test_dict={}
+for i in range(len(time_test)):
+    delta = datetime.fromtimestamp(time_test[i]) - datetime.strptime('1950-01-01','%Y-%m-%d')
+    delta = delta.days
+    adt_test_dict[delta]=adt_test[i]
+
+np.save('./Data/adt_test.npy',adt_test_dict)
+
+'''--------------------Train ADT Data--------------------'''
+adt=SLA['adt'][:]
+time=SLA['time'][:]
+adt_dict={}
+for i in range(len(time)):
+    delta = datetime.fromtimestamp(time[i]) - datetime.strptime('1950-01-01','%Y-%m-%d')
+    delta = delta.days
+    adt_dict[delta]=adt[i]
+
+np.save('./Data/adt_train.npy',adt_dict)
